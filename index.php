06a9ad3c7c0b7fb41e3a89cdfa3446c7bdd79e6c
MD RASEL BABU <57211344+RASEL-BABU@users.noreply.github.com>
2022-07-23 01:31:53 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>student management sytem</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+	<nav>
+		<label class="logo">W-School</label>
+		<ul>
+			<li><a href="">Home</a></li>
+				<li><a href="">Contact</a></li>
+					<li><a href="">Admission</a></li>
+						<li><a href="login.php"class="btn btn-success" >Login</a></li>
+						
+					</ul>
+	</nav>
+	<div class ="section1">
+		<label1 class=img_text>WE TEACH STUDENTS WITH CARE</label1>
+		<img class ="main_img"src="school_management.jpg">
+		
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<img class="welcome_img"src="school2.jpg">
+				
+			</div>
+			<div class="col-md-8">
+				<h1> Welcome to W-school</h1>
+
+<p>What is school management system project?
+Image result for school management system project python documentary
+The school management system is a web-based system which will use as a platform for interaction between student, teachers and parents. While the main objective of this project is to computerize the paperwork in the system and automate the work</p>
+			</div>
+		</div>
+		
+	</div>
+	<center>
+		<h1>Our teachers</h1>
+	</center>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<img class="teacher"src="teacher1.jpg">
+				<p>a person who teaches or instructs, especially as a profession; instructor</p>
+			</div>
+			<div class="col-md-4">
+				<img class="teacher"src="teacher2.jpg">
+				<p>a person who teaches or instructs, especially as a profession; instructor</p>
+				
+			</div>
+			<div class="col-md-4">
+				<img class="teacher"src="teacher3.jpg">
+				<p>a person who teaches or instructs, especially as a profession; instructor</p>
+			</div>
+		</div>
+	</div>
+	<center>
+		<h1>Our Courses</h1>
+	</center>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<img class="teacher"src="graphic.jpg">
+				<p>graphic design</p>
+			</div>
+			<div class="col-md-4">
+				<img class="teacher"src="marketing.png">
+				<p>marketing</p>
+				
+			</div>
+			<div class="col-md-4">
+				<img class="teacher"src="web.jpg">
+				<p>web design $ development</p>
+			</div>
+		</div>
+	</div>
+	<center>
+		<h1 class="adm">Admission form</h1>
+	</center>
+
+	
+		<div align="center" class="Admission_form">
+		
+		<form>
+			<div class="adm_int">
+				<label class="label_text">name</label>
+				<input class="input_deg" type="text" name="">
+			</div>
+			<div class="adm_int">
+				<label class="label_text">email</label>
+				<input class="input_deg" type="text" name="">
+			</div>
+			<div class="adm_int">
+				<label class="label_text">Phone</label>
+				<input class="input_deg" type="text" name="">
+			</div>
+			<div class="adm_int">
+				<label class="label_text">message</label>
+			
+			<textarea class="t_text"></textarea>
+		</div>
+		<div>
+		 <input class="btn btn-primary" id="submit" type="Submit" value="Apply">
+		</div>
+
+
+
+
+		</form>
+	<footer>
+		<h3 class="footer_text">all time reserved by Rasel Babu</h3>
+	</footer>
+
+
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login page</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+	<center>
+		<div class="form_deg">
+			<form>
+				<div>
+					<label class="label_deg">username</label>
+					<input type="text" name="username">
+				</div>
+				<div>
+					<label>password</label>
+					<input type="password" name="password">
+				</div>
+				<div>
+					
+					<input type="submit" name="submit" value="login">
+				</div>
+
+			</form>
+		</div>
+	</center>
+
+	</body>
+</html>
